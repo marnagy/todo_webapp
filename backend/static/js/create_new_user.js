3e5b8b5d78276333931fd699ae604c67db9ddf18
Marek Nagy <marek.nagy123456@gmail.com>
2024-08-10 12:31:44 +0200

    Added creation of user and log in

--- backend/static/js/create_new_user.js
+++ backend/static/js/create_new_user.js
@@ -1,19 +1,41 @@
 console.log('Hello world');
 
-function create_new_user() {
-    let username = document.getElementById('username-input').value
+async function create_new_user() {
+    const username = document.getElementById('username-input').value;
     // console.log(`Username is ${username}`);
-    let password = document.getElementById('password-input').value
+    const password = document.getElementById('password-input').value;
     // console.log(`Password is ${password}`);
-    let password_confirm = document.getElementById('confirm-password-input').value
+    const password_confirm = document.getElementById('confirm-password-input').value;
     // console.log(`Password confirm is ${password_confirm}`);
     
     if (password !== password_confirm) {
-        alert("Passwords do NOT match!")
+        alert("Passwords do NOT match!");
         return;
     }
     
     console.log("Check passed!");
-    // TODO: continue here
+    const base_url = window.location.origin;
+    const path = "/users/add";
+    const url = `${base_url}${path}`;
+    console.log(url);
     
+    // TODO: test
+
+    const resp = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            }),
+        }
+    );
+    console.log(resp);
+    console.log(resp.json());
+    
+    if (resp.status == 200) {
+        window.location.replace("/")
+    }
 }
---
+++ backend/static/js/login.js
@@ -0,0 +1,51 @@
+function setCookie(cname, cvalue, exhours=null) {
+    const d = new Date();
+    if (exhours === null) {
+        document.cookie = cname + "=" + cvalue;
+    }
+    else {
+        d.setTime(d.getTime() + (exhours*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires; // + ";path=/";
+    }
+}
+
+async function login() {
+    const username = document.getElementById('username-input').value;
+    console.log(`Username is ${username}`);
+    const password = document.getElementById('password-input').value;
+    console.log(`Password is ${password}`);
+
+    const base_url = window.location.origin;
+    const path = "/token";
+    const url = `${base_url}${path}`;
+    console.log(url);
+
+    const resp = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        }),
+    }
+    );
+
+    if (resp.status != 200) {
+        alert("Invalid username or password.");
+        return;
+    }
+    
+    const resp_json = await resp.json();
+    console.log(resp_json);
+    const token = await resp_json["access_token"];
+    
+    
+    setCookie("access_token", token, 4)
+    // document.cookie = `token=${}` //; expires=Thu, 18 Dec 2013 12:00:00 UTC;`
+    
+    window.location.replace("/home")
+
+}
